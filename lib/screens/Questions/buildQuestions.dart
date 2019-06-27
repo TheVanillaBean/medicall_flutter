bb9fc80678df5641e7066dd7a30d8137dccdbfa6
Yask0 <49133726+Yask0@users.noreply.github.com>
2019-06-27 16:36:25 -0700

    deleting unused var

--- lib/screens/Questions/buildQuestions.dart
+++ lib/screens/Questions/buildQuestions.dart
@@ -1,4 +1,3 @@
-import 'package:Medicall/models/consult_data_model.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
 
@@ -6,7 +5,6 @@ class BuildQuestions {
   buildQuestions(data, questionIndex, dynamicAdd, widget) {
     List<Widget> returnList = [];
     ValueChanged _onChanged;
-    ConsultData _consult = widget.data['consult'];
     var questions = data[questionIndex];
     var stringList = [];
 
