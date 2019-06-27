698b7bf2b87714c4e9fe7878583e9d33d1e7014a
Yask0 <49133726+Yask0@users.noreply.github.com>
2019-06-27 06:22:36 -0700

    fixed stripe adding same multiple customers

--- lib/util/stripe_payment_handler.dart
+++ lib/util/stripe_payment_handler.dart
@@ -6,13 +6,17 @@ class PaymentService {
     FirebaseAuth.instance.currentUser().then((user) {
       final DocumentReference docCardsRef =
           Firestore.instance.document("cards/" + user.uid);
-      Map<String, String> newCardEntryData = <String, String>{
-        "custId": "new",
-        "email": user.email,
-      };
-      docCardsRef.setData(newCardEntryData).whenComplete(() {
-        print("Document Added");
-      }).catchError((e) => print(e));
+      docCardsRef.snapshots().forEach((snap) {
+        if (snap.data == null) {
+          Map<String, String> newCardEntryData = <String, String>{
+            "custId": "new",
+            "email": user.email,
+          };
+          docCardsRef.setData(newCardEntryData).whenComplete(() {
+            print("Document Added");
+          }).catchError((e) => print(e));
+        }
+      });
       Firestore.instance
           .collection('cards')
           .document(user.uid)
