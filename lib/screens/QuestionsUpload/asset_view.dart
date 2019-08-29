28108b07fca5639a84c4d677a05e227c65d96304
Yask0 <49133726+Yask0@users.noreply.github.com>
2019-08-29 09:42:37 -0700

    switching back to latest flutter

--- lib/screens/QuestionsUpload/asset_view.dart
+++ lib/screens/QuestionsUpload/asset_view.dart
@@ -30,8 +30,8 @@ class AssetState extends State<AssetView> {
   }
 
 
-  void _loadImage() async {
-    byteData = await this._asset.requestThumbnail(300, 300, quality: 50);
+   void _loadImage() async {
+    byteData = await this._asset.getByteData(quality: 100);
 
     if (this.mounted) {
       setState(() {});
--- lib/screens/Registration/index.dart
+++ lib/screens/Registration/index.dart
@@ -3,7 +3,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
-import 'package:datetime_picker_formfield/datetime_picker_formfield.dart';
 import 'package:google_maps_webservice/places.dart';
 import 'package:intl/intl.dart';
 import 'package:Medicall/secrets.dart' as secrets;
