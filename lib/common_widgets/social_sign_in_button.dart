d7066bd381dcc47b7535e4f2c5a263bebec5c72f
Alex A <appdaddy@Alexs-MacBook-Pro.local>
2019-12-27 16:04:47 -0700

    Add common button and dialog widgets

---
+++ lib/common_widgets/social_sign_in_button.dart
@@ -0,0 +1,37 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+import 'custom_raised_button.dart';
+
+class SocialSignInButton extends CustomRaisedButton {
+  SocialSignInButton({
+    @required String imgPath,
+    @required String text,
+    Color color,
+    Color textColor,
+    VoidCallback onPressed,
+  })  : assert(imgPath != null),
+        assert(text != null),
+        super(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Image.asset(imgPath),
+              Text(
+                text,
+                style: TextStyle(
+                  color: textColor,
+                  fontSize: 15.0,
+                ),
+              ),
+              Opacity(
+                opacity: 0,
+                child: Image.asset(imgPath),
+              ),
+            ],
+          ),
+          color: color,
+          onPressed: onPressed,
+        );
+}
