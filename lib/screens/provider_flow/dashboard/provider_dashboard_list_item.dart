6fd49c9ce5ab682cc85ccda116a5153bf6e3de77
Yask0 <jasmin.cufurovic@gmail.com>
2020-09-10 08:21:10 -0700

    fix for profile img stretch

--- lib/screens/provider_flow/dashboard/provider_dashboard_list_item.dart
+++ lib/screens/provider_flow/dashboard/provider_dashboard_list_item.dart
@@ -20,47 +20,41 @@ class ProviderDashboardListItem extends StatelessWidget {
       return Container(
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(16.0),
-          border: Border.all(color: Colors.transparent, width: 0.5),
+          border: Border.all(
+            color: Colors.transparent,
+            width: 0.5,
+          ),
         ),
         child: ReusableCard(
-          elevation: onTap == null ? 0 : 2,
           leading: consult.patientUser.profilePic.length > 0
               ? displayProfilePicture(
                   extImageProvider, consult.patientUser.profilePic)
               : Icon(
                   Icons.account_circle,
-                  size: 60,
+                  size: 40,
                   color: Colors.grey,
                 ),
-          title: Container(
-            width: 200,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Text(
-                  '${consult.patientUser.fullName}',
-                  style: Theme.of(context).textTheme.bodyText1,
-                ),
-                SizedBox(height: 2),
-                Text('${consult.symptom}',
-                    style: Theme.of(context).textTheme.caption),
-                SizedBox(height: 2),
-              ],
-            ),
+          title: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                '${consult.patientUser.fullName}',
+                style: Theme.of(context).textTheme.bodyText1,
+              ),
+              SizedBox(height: 2),
+              Text('${consult.symptom} visit',
+                  style: Theme.of(context).textTheme.caption),
+              SizedBox(height: 2),
+            ],
           ),
           subtitle: '${consult.parsedDate}',
           trailing: Container(
+            width: 60,
             alignment: Alignment.centerLeft,
-            width: 80,
-            height: 100,
-            child: Text(
-              EnumToString.parseCamelCase(consult.state) ?? "",
-              textAlign: TextAlign.left,
-              overflow: TextOverflow.visible,
-              style: Theme.of(context).textTheme.caption.copyWith(
-                  color: Theme.of(context).colorScheme.primary,
-                  fontWeight: FontWeight.bold),
-            ),
+            child: Text(EnumToString.parseCamelCase(consult.state) ?? "",
+                style: Theme.of(context).textTheme.caption.copyWith(
+                    color: Theme.of(context).colorScheme.primary,
+                    fontWeight: FontWeight.bold)),
           ),
           onTap: onTap,
         ),
@@ -74,14 +68,14 @@ class ProviderDashboardListItem extends StatelessWidget {
   Widget displayProfilePicture(
       ExtendedImageProvider extImageProvider, String profilePicAddress) {
     return CircleAvatar(
-      radius: 25,
+      radius: 20,
       backgroundColor: Colors.grey.withAlpha(100),
       child: ClipOval(
         child: extImageProvider.returnNetworkImage(
           profilePicAddress,
           width: 100,
           height: 100,
-          fit: BoxFit.fill,
+          fit: BoxFit.cover,
         ),
       ),
     );
