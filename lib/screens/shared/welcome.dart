af45d4a160f492a858d67aa9d2bb555cdd458e07
Yask0 <jasmin.cufurovic@gmail.com>
2020-10-28 09:54:24 -0700

    intro screens initial

--- lib/screens/shared/welcome.dart
+++ lib/screens/shared/welcome.dart
@@ -3,8 +3,10 @@ import 'package:Medicall/routing/router.dart';
 import 'package:Medicall/screens/Shared/Login/login.dart';
 import 'package:Medicall/screens/patient_flow/symptoms_list/symptoms.dart';
 import 'package:Medicall/screens/provider_flow/registration/provider_registration.dart';
+import 'package:dots_indicator/dots_indicator.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:introduction_screen/introduction_screen.dart';
 
 class WelcomeScreen extends StatelessWidget {
   static Future<void> show({BuildContext context}) async {
@@ -15,17 +17,84 @@ class WelcomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     ScreenUtil.init(context);
     return Scaffold(
-      body: SingleChildScrollView(
-        padding: EdgeInsets.fromLTRB(0, 15, 0, 10),
-        child: Container(
-          height: ScreenUtil.screenHeightDp - ScreenUtil.statusBarHeight - 1,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: buildChildren(context),
+        body: Stack(
+      children: [
+        IntroductionScreen(
+          pages: [
+            PageViewModel(
+              title: "Online Visit",
+              body:
+                  "Medicall's network of local providers can help you today. No picking up the phone. No more waiting room. Your doctor will reply with a written response within 24 hours.",
+              image: Center(
+                  child: Column(
+                children: [
+                  SizedBox(
+                    width: 180,
+                    height: 140,
+                    child: Image.asset(
+                      'assets/icon/letter_mark.png',
+                    ),
+                  ),
+                  Image.asset("assets/images/doctors.jpg", height: 175.0)
+                ],
+              )),
+              // footer: ReusableRaisedButton(
+              //   title: "Login",
+              //   onPressed: () => LoginScreen.show(context: context),
+              //   width: 140,
+              //   color: Theme.of(context).disabledColor.withAlpha(70),
+              //   outlined: true,
+              // ),
+              decoration: PageDecoration(
+                pageColor: Colors.white,
+              ),
+            ),
+            PageViewModel(
+              title: "No Insurance Required",
+              body:
+                  "Low cost visit fee. No insurance headaches. Just transparent pricing.",
+              image: Center(
+                  child: Image.asset("assets/images/insurance.png",
+                      height: 175.0)),
+              decoration: PageDecoration(
+                pageColor: Colors.white,
+              ),
+            ),
+            PageViewModel(
+              title: "Free 2-day Shipping",
+              body:
+                  "If you need a prescription, we offer them at wholesale price and ship them to your door for free.",
+              image: Center(
+                  child:
+                      Image.asset("assets/images/shipping.png", height: 175.0)),
+              decoration: PageDecoration(
+                pageColor: Colors.white,
+              ),
+            ),
+          ],
+          onDone: () => SymptomsScreen.show(context: context),
+          next: Icon(Icons.navigate_next),
+          done: Text("Let's Get Started!",
+              style: TextStyle(fontWeight: FontWeight.w600)),
+          dotsDecorator: DotsDecorator(
+              size: Size.square(10.0),
+              activeSize: Size(20.0, 10.0),
+              activeColor: Theme.of(context).colorScheme.primary,
+              color: Colors.black26,
+              spacing: EdgeInsets.symmetric(horizontal: 3.0),
+              activeShape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(25.0))),
+        ),
+        Positioned(
+          bottom: 18,
+          left: 10,
+          child: FlatButton(
+            child: Text("Login"),
+            onPressed: () => LoginScreen.show(context: context),
           ),
         ),
-      ),
-    );
+      ],
+    ));
   }
 
   List<Widget> buildChildren(BuildContext context) {
